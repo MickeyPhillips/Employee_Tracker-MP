25256a805907a3232d78a9fe9924176f57ed75ed
Mickey Phillips <phillipsmickey41@gmail.com>
2021-12-02 13:22:45 -0600

    created CRUD files

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
+const create = require('./lib/create')
+const read = require('./lib/read')
+const update = require('./lib/update')
+const destroy = require('./lib/destroy')
 
 db.connect(err => {
     if(err)throw err;
     console.log('Connected to the Database.')
+    init()
 });
+const init = () => {
+    console.log('Hello I enjoy food')
+}
