d1f47fea71fdc2bede620964bc7ccc9cc7587bf4
Mickey Phillips <phillipsmickey41@gmail.com>
2021-12-02 22:40:22 -0600

    created all of the required view queries

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 require('console.table')
-
+// CTRL + F 'Create'
+// CTRL + F 'Read'
+// CTRL + F 'Update'
+// CTRL + F 'Destroy'
 db.connect(err => {
     if(err)throw err;
     console.log('Connected to the Database.')
@@ -17,6 +20,7 @@ const init = () => {
             'View all departments',
             'View all employees',
             'View all roles',
+            'Add department',
             'Quit'
         ]
     })
@@ -31,6 +35,9 @@ const init = () => {
             case 'View all roles':
                 viewRoles()
                 break
+            case 'Add department':
+                addDepartment()
+                break
             case 'Quit':
                 db.end()
                 return
@@ -38,10 +45,39 @@ const init = () => {
     })
 }
 // Create
-
+const addDepartment = () => {
+    inquirer.prompt({
+        name: 'newDepartment',
+        type: 'input',
+        message: 'What is the new Department name?',
+        validate: departmentInput => {
+            if(departmentInput) {
+                return true
+            }
+            else {
+                console.log('What is the new Department name?')
+                return false
+            }
+        }
+    })
+    .then(params => {
+        const sql = `INSERT INTO departments (name) VALUES (?)`
+        db.query(sql, params.newDepartment, (err, rows) => {
+            if(err) {
+                console.log(err)
+            }
+            viewDepartments()
+            init()
+        })
+    })
+}
 // Read
 const viewEmployees = () => {
-    const sql = `SELECT * FROM employees`
+    const sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT (manager.first_name,' ', manager.last_name) AS manager
+                FROM employees
+                LEFT JOIN roles ON employees.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                LEFT JOIN employees manager ON employees.manager_id = manager.id`
     
     db.query(sql, (err, rows) => {
         if(err) {
@@ -65,7 +101,7 @@ const viewDepartments = () => {
 }
 
 const viewRoles = () => {
-    const sql = `SELECT roles.*, departments.name AS department
+    const sql = `SELECT roles.id, roles.title, departments.name AS department, roles.salary
                 FROM roles
                 LEFT JOIN departments ON roles.department_id = departments.id`
     
